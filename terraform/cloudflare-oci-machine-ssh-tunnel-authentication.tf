aa332d658b900c30750e8b15f6db289e67830da5
Ryosuke <korygm33@gmail.com>
2023-02-28 20:32:38 +0000

    fix: attribute name

--- terraform/cloudflare-oci-machine-ssh-tunnel-authentication.tf
+++ terraform/cloudflare-oci-machine-ssh-tunnel-authentication.tf
@@ -1,12 +1,12 @@
 resource "cloudflare_workers_kv_namespace" "oci_machine_ssh_tunnel_authentication_kv" {
-  cloudflare_account_id = data.cloudflare_zone.cloudflare_main_zone.cloudflare_account_id
-  title                 = "oci-machine-ssh-tunnel-authentication-kv"
+  account_id = data.cloudflare_zone.cloudflare_main_zone.cloudflare_account_id
+  title      = "oci-machine-ssh-tunnel-authentication-kv"
 }
 
 resource "cloudflare_worker_script" "oci_machine_ssh_tunnel_authentication" {
-  cloudflare_account_id = data.cloudflare_zone.cloudflare_main_zone.cloudflare_account_id
-  name                  = "oci-machine-ssh-tunnel-authentication"
-  content               = file("../oci-machine-ssh-tunnel-authentication/index.js")
+  account_id = data.cloudflare_zone.cloudflare_main_zone.cloudflare_account_id
+  name       = "oci-machine-ssh-tunnel-authentication"
+  content    = file("../oci-machine-ssh-tunnel-authentication/index.js")
 
   kv_namespace_binding {
     # The script expects the variable KV to refer to a cloudflare KV interface
