b928882d9a618212c73e7371aea0f673a2c55f6a
Ryosuke <korygm33@gmail.com>
2023-03-13 19:40:40 +0000

    fix: data reference

--- terraform/cloudflare-cf-worker-as-openssh-ca.tf
+++ terraform/cloudflare-cf-worker-as-openssh-ca.tf
@@ -93,7 +93,7 @@ resource "cloudflare_worker_script" "cf_worker_as_openssh_ca__signer" {
 }
 
 resource "cloudflare_worker_route" "cf_worker_as_openssh_ca__signer" {
-  zone_id     = cloudflare_zone.main_zone.id
+  zone_id     = data.cloudflare_zone.main_zone.id
   pattern     = "cf-worker-as-openssh-ca--signer.kory33.net/*"
   script_name = cloudflare_worker_script.cf_worker_as_openssh_ca__signer.name
 }
